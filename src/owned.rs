43c93d323266dfd7a4c8656529b9ac005d472a9e
root <neithercouldterry@gmail.com>
2024-08-06 14:43:44 -0600

    added clone and debug implementations

--- src/owned.rs
+++ src/owned.rs
@@ -1,23 +1,32 @@
 //! Enables us to pass around owned archive types.
+//!
+//! This can be useful in many situations. For instance, suppose
+//! we want to pass Archives around in channels but we do not want
+//! to deal with complicated lifetimes.
 
+use core::fmt::Debug;
 use std::{marker::PhantomData, ops::Deref, rc::Rc, sync::Arc};
-
-use rkyv::Archive;
+use rkyv::{api::high::HighValidator, bytecheck::CheckBytes, util::AlignedVec, Archive, Portable};
 
 
 /// An owned archive type.
 #[derive(Default)]
-pub struct OwnedArchive<T, C: StableBytes> {
+pub struct OwnedArchive<T, C> {
     container: C,
     _type: PhantomData<T>
 }
 
-impl<T, C: StableBytes> OwnedArchive<T, C> {
+impl<T, C> OwnedArchive<T, C> {
     /// Creates a new OwnedArchive
     pub fn new<E>(container: C) -> Result<Self, E>
-        //where 
-         //   T: CheckBytes<Bro>
+    where 
+         //T: CheckBytes<Strategy<Validator<ArchiveValidator<'_>, SharedValidator>>> + Portable,
+        T: Portable + for<'a> CheckBytes<HighValidator<'a, E>>, 
+        E: rkyv::rancor::Source,
+        C: StableBytes
     {
+
+        rkyv::access::<T, E>(container.bytes())?;
         
         
         Ok(Self {
@@ -31,13 +40,35 @@ impl<C: StableBytes, T: Archive> Deref for OwnedArchive<T, C> {
     type Target = T::Archived;
 
     fn deref(&self) -> &Self::Target {
+        // # Safety
+        // Here we can safely access the underlying archive. This is
+        // because `StableBytes` enforces the safety contract that the underlying
+        // bytes remain stable, and thus the container that we took ownership of
+        // when creating the `OwnedArchive` has already been created.
         unsafe { rkyv::access_unchecked(self.container.bytes()) }
     }
 }
 
 
 // TODO: Implement clone and debug conditionally
+impl<T, C: Clone> Clone for OwnedArchive<T, C> {
+    fn clone(&self) -> Self {
+        Self {
+            container: self.container.clone(),
+            _type: self._type
+        }
+    }
+}
 
+
+impl<T: Archive, C: StableBytes> Debug for OwnedArchive<T, C>
+where 
+    T::Archived: Debug 
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.deref().fmt(f)
+    }
+}
 
 
 /// An interface to access stable bytes.
@@ -95,6 +126,11 @@ pub unsafe trait StableBytes {
     fn bytes(&self) -> &[u8];
 }
 
+unsafe impl StableBytes for AlignedVec {
+    fn bytes(&self) -> &[u8] {
+        self.as_ref()
+    }
+}
 
 unsafe impl StableBytes for Vec<u8> {
     fn bytes(&self) -> &[u8] {
