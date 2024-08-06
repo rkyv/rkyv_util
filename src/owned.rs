dfe3f244e8734ef101a1fb22fdfee876256336a7
root <neithercouldterry@gmail.com>
2024-08-06 14:16:51 -0600

    added dereferencing implementation

--- src/owned.rs
+++ src/owned.rs
@@ -1,10 +1,46 @@
 //! Enables us to pass around owned archive types.
 
+use std::{marker::PhantomData, ops::Deref};
+
+use rkyv::Archive;
 
 
-pub struct OwnedArchive<T, C: StableBytes> {
-    
+/// An owned archive type.
+#[derive(Default)]
+pub struct OwnedArchive<T, C: StableBytes>
+
+{
+    container: C,
+    _type: PhantomData<T>
 }
+
+impl<T, C: StableBytes> OwnedArchive<T, C> {
+    /// Creates a new OwnedArchive
+    pub fn new<E>(container: C) -> Result<Self, E>
+        //where 
+         //   T: CheckBytes<Bro>
+    {
+        
+        
+        Ok(Self {
+            container,
+            _type: PhantomData
+        })
+    }
+}
+
+impl<C: StableBytes, T: Archive> Deref for OwnedArchive<T, C> {
+    type Target = T::Archived;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { rkyv::access_unchecked(self.container.bytes()) }
+    }
+}
+
+
+// TODO: Implement clone and debug conditionally
+
+
 
 /// An interface to access stable bytes.
 ///
@@ -32,14 +68,28 @@ pub struct OwnedArchive<T, C: StableBytes> {
 ///     }
 ///
 /// }
-///
 /// ```
+/// The above code does not meet the safety contract because
+/// every other access will return a different set of bytes.
 ///
+/// Another example for safety is as follows:
+/// ```
+/// struct Good {
+///     data: Vec<u8>
+/// }
+///
+/// unsafe impl StableBytes for Good {
+///     fn bytes(&self) -> &[u8] {
+///         self.data.as_ref()
+///     }
+/// }
+/// ```
+/// The above implementation is only safe if the `Vec<u8>` inside
+/// of `Good` is never mutated. Otherwise the bytes could be changed.
 pub unsafe trait StableBytes {
     /// Gets the underlying bytes.
     fn bytes(&self) -> &[u8];
 }
-
 
 #[cfg(test)]
 mod tests {
